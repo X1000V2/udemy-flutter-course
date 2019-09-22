7dcd3a1a28b6672cf4628204722f700f1c7d07a5
Luis Barroso Vázquez <lbarrosov@gmail.com>
2019-09-22 22:04:49 +0200

    Added provider to read options from json file.

--- lib/src/pages/ComponentsPage.dart
+++ lib/src/pages/ComponentsPage.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_flutter_course/src/provider/MenuProvider.dart';
 
-class ComponentsPage extends StatefulWidget {
-  ComponentsPage({Key key}) : super(key: key);
+class ComponentsPage extends StatelessWidget {
 
-  _ComponentsPageState createState() => _ComponentsPageState();
-}
+  final options = ['Component 1','Component 2','Component 3','Component 4','Component 5'];
 
-class _ComponentsPageState extends State<ComponentsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Components")),
-      body: Container(
-        color: Colors.red,
-      ),
+      body: ListView(
+        children: _createListWithMap() 
+      )
     );
+  }
+
+  List<Widget> _createItems(){
+
+    List<Widget> listItems = List<Widget>();
+
+    options.forEach((item){
+
+      final itemView = ListTile(title: Text(item),);
+      listItems..add(itemView)
+      ..add(Divider());
+
+    });
+
+    return listItems;
+  }
+
+  List<Widget> _createListWithMap(){
+
+    menuProvider.loadData();
+
+    return options.map( (item) {
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(item),
+            subtitle: Text("data"),
+            leading: Icon(Icons.perm_contact_calendar),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: (){
+              print('Item $item clicked');
+            },
+          ),
+          Divider(),
+        ],
+      );
+    }).toList();
   }
 }
